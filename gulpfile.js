9d7c4b3a23a286f0ee2242e5f34507661cc077cb
AleksandraTerentieva <terenteva.9529@gmail.com>
2023-06-06 17:54:12 +0300

    first commit

--- gulpfile.js
+++ gulpfile.js
@@ -10,7 +10,9 @@ gulp.task('server', function() {
     browserSync({
         server: {
             baseDir: "src"
-        }
+        },
+        browser: 'chrome',
+        notify: false
     });
 
     gulp.watch("src/*.html").on('change', browserSync.reload);
@@ -20,7 +22,7 @@ gulp.task('styles', function() {
     return gulp.src("src/sass/**/*.+(scss|sass)")
         .pipe(sass({outputStyle: 'compressed'}).on('error', sass.logError))
         .pipe(rename({suffix: '.min', prefix: ''}))
-        .pipe(autoprefixer())
+        .pipe(autoprefixer({}))
         .pipe(cleanCSS({compatibility: 'ie8'}))
         .pipe(gulp.dest("src/css"))
         .pipe(browserSync.stream());
